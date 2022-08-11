c3b99e5eb0ff6471009fed5868294f1cc979b7b9
Cas9Diego <DaCastro@outlook.com>
2022-08-11 16:50:52 -0500

    Final product

--- Project6b/ViewController.swift
+++ Project6b/ViewController.swift
@@ -3,7 +3,7 @@
 //  Project6b
 //
 //  Created by Diego Castro on 11/08/22.
-//
+// AUTOLAYOUT IN CODE
 
 import UIKit
 
@@ -11,7 +11,68 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = .red
+        label1.text = "This"
+        label1.sizeToFit()
+        
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = .cyan
+        label2.text = "Cool"
+        label2.sizeToFit()
+        
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = .yellow
+        label3.text = "Awesome"
+        label3.sizeToFit()
+        
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = .green
+        label4.text = "Nice"
+        label4.sizeToFit()
+        
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = .blue
+        label5.text = "WOW"
+        label5.sizeToFit()
+        
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+        
+//        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+//
+//        for label in viewsDictionary.keys {
+//        view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//        }
+//
+//        let metrics = ["labelHeight": 80]
+//        view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary)) //On VFL, each label has a priority, with 1000 being an absolute priority, but less than 1000 is optional, so, that why we add the @999, to let xcode know that all of them are flexible.
+//
+        //The code above uses VFL to create the restrictions, but tis layout can also be created using anchors.
+        
+        var previous: UILabel?
+        
+        for label in [label1, label2, label3, label4, label5] {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true //Same width as the main view
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            //exactly 88 pixels of height
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+            }
+            previous = label
+        }
+        
     }
 
 
